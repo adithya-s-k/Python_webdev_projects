c21769eb1ee00de08fb7cc8791043382b57f6289
Adithya S K <adithyaskolavi@gmail.com>
2022-04-17 21:13:37 +0530

    Stuck at a bug 'function' object has no attribute 'method' - Adithya S K

--- Flask_projects/to_do_list_web_app/app.py
+++ Flask_projects/to_do_list_web_app/app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request,redirect
+import flask
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
+from requests import request
 app =Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer ,primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+def __repr__(self):
+    return '<Task %r>' %self.id    
+
+@app.route('/', methods=["GET","POST"])
 def index():
-    return render_template('index.html')
+    if request.method=="POST":
+        pass
+    else:
+        return render_template('index.html')
 
 if(__name__ == "__main__"):
     app.run(debug=True) 
